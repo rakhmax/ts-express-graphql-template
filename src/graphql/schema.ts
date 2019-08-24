282246c375be1ffd0f3afb7cb11e2d0d723c3c45
Maxim Rakhmanin <rakhmaxdev@gmail.com>
2019-08-24 23:39:11 +0300

    Refactor gql

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -2,8 +2,8 @@ import 'graphql-import-node';
 import { makeExecutableSchema } from "graphql-tools";
 import { GraphQLSchema } from 'graphql';
 import resolvers from './resolverMap';
-import * as rootTypeDefs from './schema.graphql';
-import * as userTypeDefs from './schemas/user.graphql';
+import * as rootTypeDefs from './schema.gql';
+import * as userTypeDefs from './schemas/user.gql';
 
 const schema: GraphQLSchema = makeExecutableSchema({
   typeDefs: [
